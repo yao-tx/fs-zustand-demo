aebbb55f09bcd656657a67656cce5075a84ab173
Yao TengXiong <yao_tengxiong@hotmail.com>
2025-01-28 01:25:02 +0800

    styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,11 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <div className="w-full container mx-auto">
+      <div className="w-full container mx-auto px-4 md:px-6">
         <div className="my-8">
           <h1 className="font-bold text-3xl text-center">Welcome to Acme Store</h1>
         </div>
-        <main className="grid grid-cols-3 gap-4 w-full">
+        <main className="grid grid-cols-1 lg:grid-cols-3 gap-4 w-full">
           <ProductList />
         </main>
       </div>
